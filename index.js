369ceafcff8854fb4df6b185ba804fabdbf556f3
Deniz Hürmeydan <denizhurmeydan@gmail.com>
2023-08-14 00:26:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const destinationURL = "http://localhost:3000/users";
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    }
+    return fetch(destinationURL, configurationObject)
+    .then(response => response.json())
+    .then(json => {
+        const result = `Name: ${json.name}<br>Email: ${json.email}<br>ID: ${json.id}`;
+        document.body.innerHTML = `<div>${result}</div>`;
+       
+        return json;
+    })
+    .catch(error => {
+        document.body.innerHTML = `${error.message}`;
+    });   
+}
+submitData("name", "email");
